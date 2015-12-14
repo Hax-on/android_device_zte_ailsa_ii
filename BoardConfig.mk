406978b5d693d5e335b007b379b0c46a0193c9e7
Scott Mertz <scott@cyngn.com>
2015-12-14 22:15:50 +0800

    chowder: use appended device tree
    Change-Id: Ia959ae49942c468ae4f2400f59ac9ff90e747201

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,7 +43,7 @@ BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=16M@0-0xffffffff
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_TAGS_OFFSET := 0x02000000
 BOARD_RAMDISK_OFFSET     := 0x02200000
 TARGET_KERNEL_ARCH := arm64
