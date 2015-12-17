c2288df46f124bda1f71d74f96949c3f029c59e3
Scott Mertz <scott@cyngn.com>
2015-12-17 10:30:42 +0800

    chowder: use multi-hal for sensors
    Change-Id: I12462c18b8ff5c5bf735ab40d228a821350f79b4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -171,6 +171,9 @@ TARGET_RIL_VARIANT := caf
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
+# Sensors
+USE_SENSOR_MULTI_HAL := true
+
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
 
