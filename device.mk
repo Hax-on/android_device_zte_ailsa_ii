bb894a40513ea64ed1118da1b2b6787164d3211f
Scott Mertz <scott@cyngn.com>
2015-12-17 11:36:10 +0800

    chowder: add gps config files
    Change-Id: I9afcb08220109bf19f363ecd229cb3a8c0bf3d81

--- device.mk
+++ device.mk
@@ -112,6 +112,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     gps.msm8996
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
+
 # irqbalance
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
